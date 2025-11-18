c77dac64cc3ef6e09f0aa99144937cc7f85fabbc
Saba Nia <snia@atb.com>
2025-11-17 23:57:12 -0500

    format

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -8,7 +8,7 @@ describe('Signup Flow', () => {
   beforeEach(() => {
     cy.intercept({ resourceType: /xhr|fetch/ }, { log: false })
     cy.fixture('users.json').then((data) => {
-       users = data;
+      users = data;
     });
     cy.fixture(language).then((langData) => {
       t = langData;
@@ -20,7 +20,7 @@ describe('Signup Flow', () => {
 
   it('User is able to successfully sign up with valid credentials', () => {
 
-   newUser = { ...users.validUser, email: `test-${Date.now()}@nesto.com` };
+    newUser = { ...users.validUser, email: `test-${Date.now()}@nesto.com` };
     cy.fillSignupForm(newUser);
     cy.createAccountButton();
 
@@ -29,7 +29,7 @@ describe('Signup Flow', () => {
 
   });
 
- 
+
   it('User is able to successfully login', () => {
 
     cy.login(newUser);
@@ -42,13 +42,13 @@ describe('Signup Flow', () => {
     cy.fillSignupForm(user);
     cy.createAccountButton();
     cy.get('[data-testid="passwordConfirmation-error-message-typography"]').should('be.visible')
-    .and('contain', t.errors.passwordMismatch);
+      .and('contain', t.errors.passwordMismatch);
   });
 
 
   it('An error message is displayed when the required fields when left empty', () => {
-    
-   cy.createAccountButton();
+
+    cy.createAccountButton();
 
     cy.get('[data-testid="first-name-error-message-typography"]').should('be.visible');
     cy.get('[data-testid="last-name-error-message-typography"]').should('be.visible');
@@ -63,28 +63,28 @@ describe('Signup Flow', () => {
     cy.fillSignupForm(user);
     cy.createAccountButton();
     cy.url().should('include', '/getaquote');
-     // API Validation 
-      cy.wait('@createAccount').then((interception) => {
-        const { response } = interception;
-        
-        expect(response.statusCode).to.eq(200);
+    // API Validation 
+    cy.wait('@createAccount').then((interception) => {
+      const { response } = interception;
 
-        expect(response.body.firstName).to.eq(user.firstName);
-        expect(response.body.lastName).to.eq(user.lastName);
-        expect(response.body.email).to.eq(user.email);
-        expect(response.body.phone).to.eq(`+1${user.phoneNumber}`);
-        expect(response.body.region).to.eq(region);
-        expect(response.body).to.have.property('id').and.to.not.be.null;
-      })
+      expect(response.statusCode).to.eq(200);
+
+      expect(response.body.firstName).to.eq(user.firstName);
+      expect(response.body.lastName).to.eq(user.lastName);
+      expect(response.body.email).to.eq(user.email);
+      expect(response.body.phone).to.eq(`+1${user.phoneNumber}`);
+      expect(response.body.region).to.eq(region);
+      expect(response.body).to.have.property('id').and.to.not.be.null;
+    })
   });
 
-    it('Validate all form fields and labels are displayed correctly', () => {
+  it('Validate all form fields and labels are displayed correctly', () => {
     cy.get('[data-testid="typography"]').should('contain', t.signup.title);
     cy.get('[data-testid="first-name-input"]').should('have.attr', 'placeholder', t.labels.firstName);;
     cy.get('[data-testid="last-name-input"]').should('have.attr', 'placeholder', t.labels.lastName);
     cy.get('[data-testid="phoneInput"]').should('have.attr', 'placeholder', t.labels.phone);
     cy.get('[data-testid="region-select"]').should('contain', t.labels.province);
-    cy.get('[data-testid="email-input"]').should('have.attr', 'placeholder',t.labels.email);
+    cy.get('[data-testid="email-input"]').should('have.attr', 'placeholder', t.labels.email);
     cy.get('[data-testid="password-input"]').should('have.attr', 'placeholder', t.labels.password);
     cy.get('[data-testid="passwordConfirmation-input"]').should('have.attr', 'placeholder', t.labels.confirmPassword);
     cy.get('[data-testid="submit-button"]').should('contain', t.buttons.createAccount);
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,28 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('enterFirstName', (firstname) => {
     cy.get('[data-testid="first-name-input"]').clear().type(firstname)
@@ -49,7 +24,7 @@ Cypress.Commands.add('enterPassword', (password) => {
 })
 
 Cypress.Commands.add('confirmPassword', (confirmPassword) => {
-    cy.get('[data-testid="passwordConfirmation-input"]').clear().type(confirmPassword,  { log: false })
+    cy.get('[data-testid="passwordConfirmation-input"]').clear().type(confirmPassword, { log: false })
 })
 
 Cypress.Commands.add('createAccountButton', () => {
@@ -72,5 +47,5 @@ Cypress.Commands.add('login', (user) => {
         cy.get('input[name="email"]').clear().type(user.email);
         cy.get('input[name="password"]').clear().type(user.password, { log: false });
         cy.get('button[type="submit"]').click();
-    })  
+    })
 });
